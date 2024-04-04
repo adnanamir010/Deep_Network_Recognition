71c9c47fe2b558dcd23c5394488b84b051d014a5
adnanamir010 <adnanamir010@gmail.com>
2024-04-03 22:04:40 -0400

    Final Edits to report and code

--- .ipynb_checkpoints/networks-checkpoint.py
+++ .ipynb_checkpoints/networks-checkpoint.py
@@ -5,26 +5,31 @@ import numpy as np
 
 class MyNetwork(nn.Module):
     """
-    Creates a LenNet based architecture to
-    classify digits in Mnist dataset.
+    A custom neural network architecture for image classification.
+
+    This network consists of two convolutional blocks followed by a classifier. Each convolutional block
+    applies a convolution operation, followed by max pooling and a ReLU activation function. The first block
+    moves from 1 input channel to 10 output channels, and the second block increases this to 20 output channels.
+    A dropout layer is also included in the second block to reduce overfitting. The classifier is a sequence of
+    fully connected layers that flattens the output of the convolutional blocks and then applies linear transformations
+    to achieve the final class predictions.
+
+    Attributes:
+    - block_1 (nn.Sequential): First convolutional block.
+    - block_2 (nn.Sequential): Second convolutional block with dropout.
+    - classifier (nn.Sequential): Classifier that flattens and transforms the output into class predictions.
     """
+
     def __init__(self):
         super().__init__()
         self.block_1 = nn.Sequential(
-            nn.Conv2d(
-            in_channels=1,
-            out_channels=10,
-            kernel_size=5),
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU()
         )
 
         self.block_2 = nn.Sequential(
-            nn.Conv2d(
-            in_channels=10,
-            out_channels=20,
-            kernel_size=5,
-            ),
+            nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5),
             nn.Dropout2d(0.5),
             nn.MaxPool2d(kernel_size=2),
             nn.ReLU()
@@ -37,12 +42,35 @@ class MyNetwork(nn.Module):
         )
 
     def forward(self, x):
+        """
+        Defines the forward pass of the network.
+
+        Parameters:
+        - x (Tensor): The input tensor.
+
+        Returns:
+        - Tensor: The output of the network after applying log softmax on the classifier's output.
+        """
         x = self.block_1(x)
         x = self.block_2(x)
         x = self.classifier(x)
         return nn.functional.log_softmax(x, dim=1)
 
 class GaborLayer(nn.Module):
+    """
+    A convolutional layer using Gabor filters as fixed parameters.
+
+    This layer initializes Gabor filters based on the specified number of input and output channels and the kernel size.
+    The filters are used in a fixed convolution operation that does not update during training, simulating the effect of
+    Gabor filters on the input image.
+
+    Attributes:
+    - in_channels (int): Number of input channels.
+    - out_channels (int): Number of output channels.
+    - kernel_size (int): Size of the convolution kernel.
+    - filters (Tensor): The initialized Gabor filters.
+    """
+
     def __init__(self, in_channels, out_channels, kernel_size):
         super(GaborLayer, self).__init__()
         self.in_channels = in_channels
@@ -53,6 +81,12 @@ class GaborLayer(nn.Module):
         self.filters = nn.Parameter(self.create_gabor_filters(), requires_grad=False)
         
     def create_gabor_filters(self):
+        """
+        Creates Gabor filters for convolution.
+
+        Returns:
+        - Tensor: A tensor containing the initialized Gabor filters.
+        """
         num_orientations = self.out_channels // self.in_channels
         gabor_filters = []
         
@@ -75,10 +109,33 @@ class GaborLayer(nn.Module):
         return filters_tensor
 
     def forward(self, x):
+        """
+        Applies Gabor filters to the input.
+
+        Parameters:
+        - x (Tensor): The input tensor.
+
+        Returns:
+        - Tensor: The output tensor after applying the Gabor filters.
+        """
         x = nn.functional.conv2d(x, self.filters, padding=self.kernel_size//2)
         return x
 
 class MyGaborNetwork(nn.Module):
+    """
+    A custom neural network that incorporates Gabor filters at the first layer.
+
+    This network uses a GaborLayer as its first block to process input images, aiming to capture texture
+    and orientation features efficiently. Following the GaborLayer, a conventional convolutional block
+    applies further transformations. The network dynamically calculates the necessary input features for
+    the linear layer based on a dummy forward pass, ensuring compatibility with various input sizes.
+
+    Attributes:
+    - block_1 (GaborLayer): The initial layer using Gabor filters.
+    - block_2 (nn.Sequential): A standard convolutional block including dropout, max pooling, and ReLU activation.
+    - classifier (nn.Sequential): A classifier that includes a fully connected network with a ReLU activation.
+    """
+
     def __init__(self, input_size=(1, 28, 28)):
         super(MyGaborNetwork, self).__init__()
         self.block_1 = GaborLayer(in_channels=1, out_channels=10, kernel_size=5)
@@ -98,16 +155,34 @@ class MyGaborNetwork(nn.Module):
         self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Linear(in_features, 50),
-            nn.ReLU(),  # Added ReLU here for better network performance
+            nn.ReLU(),  # Added ReLU for improved performance
             nn.Linear(50, 10)
         )
 
     def forward_features(self, x):
+        """
+        Processes input through the initial blocks to determine feature size for the classifier.
+
+        Parameters:
+        - x (Tensor): The input tensor.
+
+        Returns:
+        - Tensor: The output tensor from the initial processing blocks.
+        """
         x = self.block_1(x)
         x = self.block_2(x)
         return x
 
     def forward(self, x):
+        """
+        Defines the forward pass of the network with Gabor filters.
+
+        Parameters:
+        - x (Tensor): The input tensor.
+
+        Returns:
+        - Tensor: The output of the network after applying log softmax on the classifier's output.
+        """
         x = self.forward_features(x)
         x = self.classifier(x)
         return nn.functional.log_softmax(x, dim=1)
--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -8,7 +8,27 @@ from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from networks import MyNetwork
+import cv2
+import numpy as np
 
+"""
+    Train the network for one epoch.
+    
+    This function iterates over the training dataset using the provided DataLoader,
+    computes the loss for each batch, and updates the model parameters. It tracks and
+    returns the average training loss for the epoch.
+    
+    Parameters:
+    - model: The neural network model to be trained.
+    - device: The computing device (CPU or GPU) where the training is performed.
+    - train_loader (DataLoader): DataLoader for the training dataset.
+    - optimizer: The optimization algorithm used to update the model parameters.
+    - epoch (int): The current training epoch number.
+    
+    Returns:
+    - average_loss (float): The average loss over the training dataset for the epoch.
+    - total_data (int): Total number of samples in the training dataset.
+"""
 def train_network(model, device, train_loader, optimizer, epoch):
     model.train()
     total_loss = 0
@@ -23,6 +43,24 @@ def train_network(model, device, train_loader, optimizer, epoch):
     average_loss = total_loss / len(train_loader)
     return average_loss, len(train_loader.dataset)
 
+
+"""
+    Evaluate the network on the test dataset.
+
+    This function iterates over the test dataset, computes the loss for each batch,
+    and calculates the overall test loss and accuracy of the model. It sets the model
+    to evaluation mode before testing, disables gradient computations, and prints out
+    the average test loss and accuracy.
+
+    Parameters:
+    - model: The neural network model to be evaluated.
+    - device: The computing device (CPU or GPU) where the evaluation is performed.
+    - test_loader (DataLoader): DataLoader for the test dataset.
+
+    Returns:
+    - test_loss (float): The average loss over the test dataset.
+    - test_accuracy (float): The accuracy of the model on the test dataset.
+"""
 def test_network(model, device, test_loader):
     model.eval()  # Set the model to evaluation mode
     test_loss = 0
@@ -44,14 +82,49 @@ def test_network(model, device, test_loader):
 
     return test_loss, test_accuracy
 
-def visualize_data(test_loader,nums):
-    dataiter = iter(test_loader)
-    images, labels = next(dataiter)  # Corrected usage
-    images = images.numpy()
+"""
+    Process an input image and predict the output using a specified model.
 
-    fig = plt.figure(figsize=(9, 3))
-    for idx in range(nums):
-        ax = fig.add_subplot(1, nums, idx+1, xticks=[], yticks=[])
-        ax.imshow(images[idx].squeeze(), cmap='gray')
-        ax.set_title(str(labels[idx].item()))
-    plt.show()
+    This function reads an image from a given path in grayscale, optionally inverts it,
+    resizes it to 28x28 pixels, normalizes its pixel values to the range [0, 1], applies
+    MNIST-specific normalization, and then passes the processed image tensor through the
+    given model to get the prediction.
+
+    Parameters:
+    - image_path (str): The file path to the image to process.
+    - model: The prediction model to use.
+    - device: The device (CPU or GPU) to use for computations.
+    - invert (bool): If True, invert the colors of the image.
+
+    Returns:
+    - The output from the model prediction.
+"""
+def process_and_predict(image_path, model, device,invert):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Read the image in grayscale
+    if invert:
+        image = cv2.bitwise_not(image)
+    image = cv2.resize(image, (28, 28))  # Resize to 28x28
+    image = image.astype(np.float32) / 255.0  # Normalize image pixels to [0, 1]
+    tensor = torch.tensor(image).unsqueeze(0).unsqueeze(0).to(device)  # Add batch and channel dimensions
+    tensor = (tensor - 0.1307) / 0.3081  # Apply MNIST normalization
+    output = model(tensor) # Run inference/prediction
+    return output
+
+"""
+    A transformation class for preprocessing greek alphabet images provided with this course . This class applies
+    a series of transformations to convert an input image to grayscale, scale it
+    maintaining aspect ratio, perform a center crop, and finally invert its colors.
+
+    The sequence of transformations is as follows:
+    1. Convert the image to grayscale to focus on shape and texture rather than color.
+    2. Scale the image to a specific ratio (simulating the aspect ratio found in Greek art) without rotation.
+    3. Center crop the image to a fixed size (28x28), highlighting the central figure or motif.
+    4. Invert the image colors, creating a visual style reminiscent of ancient Greek pottery.
+
+"""
+class GreekTransform:
+    def __call__(self, x):
+        x = transforms.functional.rgb_to_grayscale(x)
+        x = transforms.functional.affine(x, 0, (0, 0), 36/128, 0)
+        x = transforms.functional.center_crop(x, (28, 28))
+        return transforms.functional.invert(x)
